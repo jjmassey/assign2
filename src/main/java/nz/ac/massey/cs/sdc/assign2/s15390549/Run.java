7aa6573024feb1cb222261005e0d00309cda73dc
jjmassey <joshua.fleet.1@uni.massey.ac.nz>
2018-10-13 09:14:58 +1300

    test velocity branch

--- src/main/java/nz/ac/massey/cs/sdc/assign2/s15390549/Run.java
+++ src/main/java/nz/ac/massey/cs/sdc/assign2/s15390549/Run.java
@@ -1,5 +1,7 @@
 package nz.ac.massey.cs.sdc.assign2.s15390549;
 
+// branch velocity test
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -10,37 +12,37 @@ import org.apache.log4j.spi.LoggingEvent;
 import nz.ac.massey.cs.sdc.assign2.s15390549.MemAppender;
 
 public class Run {
-	
+
 	private static final LoggingEvent LoggingEvent = null;
-	
+
 	public static Logger fileLogger = Logger.getLogger("--> File");
-	
+
 	public static void main(String[] args) {
-		
+
 		List<LoggingEvent> events = new ArrayList<LoggingEvent>();
 		List<LoggingEvent> immutablelist = Collections.unmodifiableList(events);
-		
-		
+
+
 		// Test to see if logs are saved to FileLoggers
 		MemAppender object = MemAppender.getInstance();
 		fileLogger.addAppender(object);
-		
+
 		fileLogger.info("transactions imported");
 		fileLogger.info("test");
 		fileLogger.info("test2");
 		fileLogger.info("test3");
-		
+
 		System.out.print("Current Logs: " + object.getCurrentLogs());
-		
+
 		// Test to see if discardedLogs works
 		System.out.print("\n\nNumber of Discarded Logs: " + object.getDiscardedLogCount());
-		
+
 		// Test to try to add to immutable list
 //		immutablelist.add(LoggingEvent);
-		
+
 		// Test to add to mutable list
-		events.add(LoggingEvent); 
-		
+		events.add(LoggingEvent);
+
 	}
 
 }
