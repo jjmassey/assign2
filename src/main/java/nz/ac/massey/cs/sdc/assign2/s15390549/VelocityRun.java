aa0910d7442012393956fe110495efd8fc6cf7e2
jjmassey <joshua.fleet.1@uni.massey.ac.nz>
2018-10-18 14:13:00 +1300

    velocityrun

--- src/main/java/nz/ac/massey/cs/sdc/assign2/s15390549/VelocityRun.java
+++ src/main/java/nz/ac/massey/cs/sdc/assign2/s15390549/VelocityRun.java
@@ -1,21 +1,28 @@
 package nz.ac.massey.cs.sdc.assign2.s15390549;
 
+import java.io.IOException;
+
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.FileAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import nz.ac.massey.cs.sdc.assign2.s15390549.VelocityLayout;
+import nz.ac.massey.cs.sdc.assign2.s15390549.MemAppender;
 
 public class VelocityRun {
 
-	public static Logger testLogger = Logger.getLogger("--> File");
+	public static Logger testLogger = Logger.getLogger(" testLogger ");
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		BasicConfigurator.configure();
 		
 		MemAppender object = MemAppender.getInstance();
-		testLogger.addAppender(object);
+		testLogger.addAppender(new FileAppender(new VelocityLayout(), "logs.txt"));
+		testLogger.info("Test0");
+		testLogger.info("Test1");
+		
 		
 		testLogger.setLevel(Level.ALL);
 
