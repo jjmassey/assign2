bf69810ab554f3d632856f5b1af99f9e688bee0c
jjmassey <joshua.fleet.1@uni.massey.ac.nz>
2018-10-22 13:41:38 +1300

    Stress tests start

--- src/test/java/nz/ac/massey/cs/sdc/assign2/s15390549/StressTest.java
+++ src/test/java/nz/ac/massey/cs/sdc/assign2/s15390549/StressTest.java
@@ -1,55 +1,99 @@
 package nz.ac.massey.cs.sdc.assign2.s15390549;
 
-import java.io.IOException;
+import static org.junit.Assert.assertNull;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Date;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.FileAppender;
 import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggingEvent;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
 public class StressTest {
-
-	MemAppender object = MemAppender.getInstance();
-	public static Logger testLogger = Logger.getLogger(" testLogger ");
 	
-	@org.junit.Test
-	public void stressTest() {
-		try {
-			testLogger.addAppender(new FileAppender(new VelocityLayout(), "stressLogs.txt"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		for (int i=0; i<10; i++) {
-			testLogger.info("Test: " + i);
-		}
-	}
+	ArrayList<LoggingEvent> events1;
+	LinkedList<LoggingEvent> events2;
+	private MemAppender object;
+	private MemAppender object1;
+	private MemAppender object2;
+	private Logger testLogger;
+	private Logger linkedListLogger;
+	private Logger arrayListLogger;
+	Date start;
+	Date end;
 	
-	@org.junit.Test
-	public void linkedListTest() {
-		
-	}
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		BasicConfigurator.configure();
 	}
+	
+	@Before
+	public void setUp() throws Exception {
+		object = MemAppender.getInstance();
+		events1 = new ArrayList<LoggingEvent>();
+		events2 = new LinkedList<LoggingEvent>();
+		object1 = new MemAppender(events1);			// Array list
+		object2 = new MemAppender(events2);			// Linked list
+		arrayListLogger = Logger.getLogger("---> Array list logger");
+		linkedListLogger = Logger.getLogger("---> Linked list logger");
+		start = new Date();
+		end = new Date();
+	}
+	
+	@org.junit.Test
+	public void arrayList() throws IOException {
+		arrayListLogger.addAppender(object1);
+		System.out.println(start.toString());
+		for (int i=0; i<100; i++) {
+			arrayListLogger.info("Array Test: " + i);
+		}
+		System.out.println(end.toString());
+	}
+	
+	@org.junit.Test
+	public void linkedList() throws IOException {
+		linkedListLogger.addAppender(object1);
+		System.out.println(start.toString());
+		for (int i=0; i<100; i++) {
+			linkedListLogger.info("Linked Test: " + i);
+		}
+		System.out.println(end.toString());
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		object = null;
+		object1 = null;
+		object2 = null;
+		events1 = null;
+		events2 = null;
+		testLogger = null;
+		arrayListLogger = null;
+		linkedListLogger = null;
+		start = null;
+		end = null;
+		assertNull(object);
+		assertNull(object1);
+		assertNull(object2);
+		assertNull(events1);
+		assertNull(events2);
+		assertNull(testLogger);
+		assertNull(linkedListLogger);
+		assertNull(arrayListLogger);
+		assertNull(start);
+		assertNull(end);
+	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		
-	}
-
-	@Before
-	public void setUp() throws Exception {
-		
-	}
-
-	@After
-	public void tearDown() throws Exception {
 		
 	}
 }
--- src/test/java/nz/ac/massey/cs/sdc/assign2/s15390549/Test.java
+++ src/test/java/nz/ac/massey/cs/sdc/assign2/s15390549/Test.java
@@ -1,11 +1,14 @@
 package nz.ac.massey.cs.sdc.assign2.s15390549;
 
-import static org.junit.Assert.*;
-import static org.junit.Assume.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.log4j.BasicConfigurator;
@@ -18,71 +21,137 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
-import nz.ac.massey.cs.sdc.assign2.s15390549.MemAppender;
-//import nz.ac.massey.cs.sdc.assign2.s15390549.VelocityLayout;
 
 public class Test {
 	
-	// Do we need this?	
-//	BasicConfigurator.configure();
+
+	private MemAppender object;
+	private MemAppender object1;
+	private MemAppender object2;
+	private MemAppender object3;
+	private MemAppender object4;
+	private FileAppender fAppender;
+	private ConsoleAppender cAppender;
+	List<LoggingEvent> immutableList;
+	ArrayList<LoggingEvent> events1;
+	LinkedList<LoggingEvent> events2;
+	String pattern;
+	private Logger testLogger;
+	private Logger testLogger1;
+	private Logger testLogger2;
+	private final LoggingEvent LoggingEvent = null;
 	
-	// Create two instances of MemAppender
-	MemAppender object = MemAppender.getInstance();
-	MemAppender object1 = MemAppender.getInstance();
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		BasicConfigurator.configure();
+	}
 	
-	// Use getCurrentLogs() to return an immutable list
-	List<LoggingEvent> immutableList = object.getCurrentLogs();
+	@Before
+	public void setUp() throws Exception {
+		BasicConfigurator.configure();
+		System.out.println("Setting it up!");
+		object = new MemAppender();
+		events1 = new ArrayList<LoggingEvent>();
+		events2 = new LinkedList<LoggingEvent>();
+		object1 = new MemAppender(events1);
+		object2 = new MemAppender(events2);
+		object3 = MemAppender.getInstance();
+		object4 = MemAppender.getInstance();
+		pattern = "Timestamp: ${d} ${p} [${t}] ${c} ${m}${n}";
+		fAppender = new FileAppender(new VelocityLayout(pattern),"fileLogs.txt");
+		cAppender = new ConsoleAppender(new VelocityLayout(pattern));
+		immutableList = object.getCurrentLogs();
+		testLogger = Logger.getLogger("---> Test logger");
+		testLogger1 = Logger.getLogger("---> File logger");
+		testLogger2 = Logger.getLogger("---> Console logger");
+		
+	}
 	
-	// Create a mutable list
-	ArrayList<LoggingEvent> events = new ArrayList<LoggingEvent>();
-	
-	// Create an instance of a logging event
-	private static final LoggingEvent LoggingEvent = null;
-	private static final LoggingEvent LoggingEvent1 = null;
-	private static final LoggingEvent LoggingEvent2 = null;
-	
-	// Create a logger - testLogger
-	public static Logger testLogger = Logger.getLogger("--> File");
-	
-	
-	// FileAppender with MemAppender Combination / getCurrentLogs Test
+	// LinkedList as parameter for MemAppender Test
 	@org.junit.Test
-	public void comboTest() throws IOException {
-		MemAppender object = MemAppender.getInstance();
-		testLogger.addAppender(object);
-		List<LoggingEvent> immutableList = object.getCurrentLogs();
-		assumeTrue(immutableList.size() == 0);
-		testLogger.info("test0");
-		assertTrue(immutableList.size()==1);	
+	public void LinkedList() throws IOException {
+		testLogger.addAppender(object1);
+		assumeTrue(events1.size() == 0);
+		testLogger.info("Linked list test");
+		assertTrue(events1.size()==1);
+		
+	}
+	
+	// ArrayList as parameter for MemAppender Test
+	@org.junit.Test
+	public void ArrayList() throws IOException {
+		testLogger.addAppender(object2);
+		assumeTrue(events2.size() == 0);
+		testLogger.info("Array list test");
+		assertTrue(events2.size()==1);
+		
 	}
 	
 	// FileAppender with VelocityLayout Combination 
 	@org.junit.Test
-	public void comboTest1() throws IOException {
-
+	public void fileAppender() throws IOException {
+		testLogger1.addAppender(fAppender);
+		testLogger1.info("File Appender Test");
+		// WORKS - Can be found in the generated file fileLogs.txt
+		// Don't know how to do a j unit test for this though??
 	}
 	
 	// ConsoleAppender with VelocityLayout Combination 
 	@org.junit.Test
-	public void comboTest2() throws IOException {
-
+	public void consoleAppender() throws IOException {
+		testLogger2.addAppender(cAppender);
+		testLogger2.info("Console Appender Test");
+		System.out.println("######## Console Appender Test ########");
+		// WORKS - Can be found within all the statements in the console!!
+		// Don't know how to do a j unit test for this though??
+	}
+	
+	// MemAppender wih INFO level Combination / getCurrentLogs Test
+	@org.junit.Test
+	public void infoLevel() throws IOException {
+		testLogger.addAppender(object);
+		assumeTrue(immutableList.size() == 0);
+		testLogger.info("Info test");
+		assertTrue(immutableList.size()==1);	
+	}
+	
+	// MemAppender with WARN level Combination
+	@org.junit.Test
+	public void warnLevel() throws IOException {
+		testLogger.addAppender(object);
+		assumeTrue(immutableList.size() == 0);
+		testLogger.warn("Warn test");
+		assertTrue(immutableList.size()==1);	
+	}
+	
+	// MemAppender with ERROR level Combination 
+	@org.junit.Test
+	public void errorLevel() throws IOException {
+		testLogger.addAppender(object);
+		assumeTrue(immutableList.size() == 0);
+		testLogger.error("Error test");
+		assertTrue(immutableList.size() == 1);	
+	}
+	
+	// MemAppender with DEBUG level Combination 
+	@org.junit.Test
+	public void debugLevel() throws IOException {
+		testLogger.addAppender(object);
+		assumeTrue(immutableList.size() == 0);
+		testLogger.debug("Debug test");
+		assertTrue(immutableList.size() == 1);	
 	}
 	
 	// Singleton Pattern Test - ensure only one instance can be created 
 	@org.junit.Test
 	public void singletonTest() {
-		// Create two instances of MemAppender
-		MemAppender object = MemAppender.getInstance();
-		MemAppender object1 = MemAppender.getInstance();
 		System.out.println("singletonTest");
-		assertTrue(object == object1);
+		assertTrue(object3 == object4);
 	}
 	
 	// Immutable List Test (METHOD 1) - ensure immutableList cannot be altered
 	@org.junit.Test
 	public void immutableTest1() {
-		MemAppender object = MemAppender.getInstance();
-		List<LoggingEvent> immutableList = object.getCurrentLogs();
 		try{
 				immutableList.add(LoggingEvent);
 				System.out.println("ImmutableTest1 - Try");
@@ -102,31 +171,50 @@ public class Test {
 	// maxSize test - (ASSUMING maxSize = 2;
 	@org.junit.Test
 	public void maxSize() {
-		MemAppender object = new MemAppender(events);
 		testLogger.addAppender(object);
 		assumeTrue(object.getDiscardedLogCount() == 0);
-		testLogger.info("test0");
-		testLogger.info("test1");
-		testLogger.info("test2");
+		testLogger.debug("Log count test");
+		testLogger.debug("Log count test");
+		testLogger.debug("Log count Test");
 		assertTrue(object.getDiscardedLogCount() == 1);
 	}
-				
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		BasicConfigurator.configure();
-	}
+	
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
 
 	@After
 	public void tearDown() throws Exception {
+		System.out.println("Running: tearDown");
+		object = null;
+		object1 = null;
+		object2 = null;
+		object3 = null;
+		object4 = null;
+		events1 = null;
+		events2 = null;
+		fAppender = null;
+		cAppender = null;
+		testLogger = null;
+		testLogger1 = null;
+		testLogger2 = null;
+		immutableList = null;
+		assertNull(object);
+		assertNull(object1);
+		assertNull(object2);
+		assertNull(object3);
+		assertNull(object4);
+		assertNull(events1);
+		assertNull(events2);
+		assertNull(fAppender);
+		assertNull(cAppender);
+		assertNull(testLogger);
+		assertNull(testLogger1);
+		assertNull(testLogger2);
+		assertNull(immutableList);
 	}
-
-	// == same identity, or .equals
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		
+	}
+	
 }
